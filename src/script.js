1d35fd4ce000a92210fd7e99f7113acfe6c6dd87
Tracy Njoroge <tracywangari997@gmail.com>
2026-02-02 23:26:59 +0300

    Store day index in dataset and toggle array values on click

--- src/script.js
+++ src/script.js
@@ -4,17 +4,33 @@ const containerEl = document.querySelector(".checkbox-container");
 const habit = {
         name: "Exercise",
         days: [false, false, false, false, false, false,false]
-    };
+};
+
 
 habitName.textContent = habit.name;
 
 const days = habit.days;
+console.log(days);
 
-days.forEach(day => {
+days.forEach((day, index) => {
     const dayEl = document.createElement("input");
-    dayEl.setAttribute("type", "checkbox");
+    dayEl.type = "checkbox";
+    dayEl.dataset.dayIndex = index;
 
     containerEl.appendChild(dayEl);
+
+    dayEl.addEventListener("change", function(e) {
+        let dayIndex = e.target.dataset.dayIndex;
+        days[dayIndex] = !days[dayIndex];
+
+    console.log(days)
+
+    })
+
 });
+
+
+
+
 
 
